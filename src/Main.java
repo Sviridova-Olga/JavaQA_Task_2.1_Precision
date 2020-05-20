87ba0d4d0146b58e77d2daa011dc3d07e5b0672d
SvOlga <sviridova-olga.iccf@yandex.ru>
2020-05-20 16:56:24 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BigDecimal regularBonus = BigDecimal.valueOf(0.3);
+        BigDecimal specialBonus = BigDecimal.valueOf(0.6);
+        BigDecimal totalBonus;
+        totalBonus = regularBonus.add( specialBonus);
+        System.out.println(totalBonus);
+
+
+    }
+
+
+    }
+
+
+
+
