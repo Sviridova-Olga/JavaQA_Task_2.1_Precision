93849ad805a8d173cb876d3e713c4ab68a420bf7
SvOlga <sviridova-olga.iccf@yandex.ru>
2020-05-20 17:41:19 +0300

    Round totalBonus

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
         double regularBonus = 0.3;
         double specialBonus = 0.6;
         double totalBonus = regularBonus + specialBonus;
-        System.out.println(totalBonus);
+        DecimalFormat f = new DecimalFormat("##.##");
+        System.out.println(f.format(totalBonus));
     }
 }
 
